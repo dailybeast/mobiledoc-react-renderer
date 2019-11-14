a48bf4ec07be66aa89235723e8cbab213a470946
Joe Toh Djojo <joseph.tohdjojo@gmail.com>
2019-11-15 04:08:25 +0800

    adds the ability to add a classname to the wrapping div (#12)
    * adds the ability to add a classname to the wrapping div
    
    * Allow className in options
    
    * Allow setting full className

--- src/renderers/0-3.js
+++ src/renderers/0-3.js
@@ -13,8 +13,16 @@ export const MARKUP_MARKER_TYPE = 0;
 export const ATOM_MARKER_TYPE = 1;
 
 export default class Renderer {
-  constructor (mobiledoc, { atoms = [], cards = [], sections = [], markups = [], additionalProps = {} }) {
+  constructor (mobiledoc, {
+    atoms = [],
+    cards = [],
+    sections = [],
+    markups = [],
+    className = 'Mobiledoc',
+    additionalProps = {}
+  }) {
     this.mobiledoc = mobiledoc;
+    this.className = className;
     this.atoms = atoms;
     this.cards = cards;
     this.markups = markups;
@@ -25,7 +33,7 @@ export default class Renderer {
   }
 
   render () {
-    const renderedSections = <div className='Mobiledoc'>{this.renderSections()}</div>;
+    const renderedSections = <div className={this.className}>{this.renderSections()}</div>;
     this.renderCallbacks.forEach(cb => cb());
 
     return renderedSections;
